38752cf07cbcef939c18ad57a49e31d2e287cdba
jaz <menesesdjaz@gmail.com>
2024-09-03 16:16:14 -0400

    agregando form a contacto

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,11 +1,19 @@
 <template>
 <section class="container" id="productos">
   <h1 class=" text-center">Contacto</h1>
-  
+  <form action="">
+    <div class="mb-3">
+      <label for="exampleFormControlInput1" class="form-label">Email address</label>
+      <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+    </div>
+    <div class="mb-3">
+     <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+     <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    </div>
+  </form>
   <div class="row">
     <button @click="volverAlInicio" class="btn btn-primary" >Ir a Inicio</button>
   </div>
-  
     <RouterView />
 </section>
 </template>
